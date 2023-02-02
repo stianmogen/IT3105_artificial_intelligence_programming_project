195e0b981c22105f083c2bbe0e5bdcd409f71f8e
Stian Mogen <stianfmo@stud.ntnu.no>
2023-02-02 11:10:11 +0100

    Update Nim.py

--- nim/Nim.py
+++ nim/Nim.py
@@ -1,23 +1,29 @@
-def nim_game(pile, max_sticks):
-    print("Current pile: {}".format(pile))
-    while pile:
+def nim_game(piles, max_sticks):
+    print("Current piles: {}".format(piles))
+    while any(piles):
         print("Player 1 turn")
-        sticks_taken = int(input("Choose sticks (1-{}): ".format(max_sticks)))
-        while sticks_taken > max_sticks or sticks_taken > pile:
-            sticks_taken = int(input("Invalid choice, Choose sticks (1-{}): ".format(min(pile, max_sticks))))
-        pile -= sticks_taken
-        print("Current pile: {}".format(pile))
-        if not pile:
+        pile = int(input("Choose pile (1-3): "))
+        while pile < 1 or pile > 3 or piles[pile-1] == 0:
+            pile = int(input("Invalid pile, choose pile (1-3): "))
+        sticks_taken = int(input("Choose sticks (1-{}): ".format(min(piles[pile-1], max_sticks))))
+        while sticks_taken < 1 or sticks_taken > max_sticks or sticks_taken > piles[pile-1]:
+            sticks_taken = int(input("Invalid choice, choose sticks (1-{}): ".format(min(piles[pile-1], max_sticks))))
+        piles[pile-1] -= sticks_taken
+        print("Current piles: {}".format(piles))
+        if not any(piles):
             print("Player 1 wins!")
             return
         print("Player 2 turn")
-        sticks_taken = int(input("Choose sticks (1-{}): ".format(max_sticks)))
-        while sticks_taken > max_sticks or sticks_taken > pile:
-            sticks_taken = int(input("Invalid choice, Choose sticks (1-{}): ".format(min(pile, max_sticks))))
-        pile -= sticks_taken
-        print("Current pile: {}".format(pile))
-        if not pile:
+        pile = int(input("Choose pile (1-3): "))
+        while pile < 1 or pile > 3 or piles[pile-1] == 0:
+            pile = int(input("Invalid pile, choose pile (1-3): "))
+        sticks_taken = int(input("Choose sticks (1-{}): ".format(min(piles[pile-1], max_sticks))))
+        while sticks_taken < 1 or sticks_taken > max_sticks or sticks_taken > piles[pile-1]:
+            sticks_taken = int(input("Invalid choice, choose sticks (1-{}): ".format(min(piles[pile-1], max_sticks))))
+        piles[pile-1] -= sticks_taken
+        print("Current piles: {}".format(piles))
+        if not any(piles):
             print("Player 2 wins!")
             return
 
-nim_game(20, 4)
+nim_game([5, 10, 15], 3)
