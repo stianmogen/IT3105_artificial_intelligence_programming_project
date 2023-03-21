1b2bcba9a0b774b9c8dc9d7c507581a3a138e7ac
Stian Mogen <stianfmo@stud.ntnu.no>
2023-03-21 13:38:47 +0100

    tournament

--- hex/tournament.py
+++ hex/tournament.py
@@ -1,12 +1,11 @@
 import os
 import random
 
-import numpy as np
-import torch
-
 from hex import HexGameState
 from hex import MCTSAgent
 from nn.qNetwork import DQN
+from nn.anet import Anet2
+from keras.models import load_model
 
 
 def run_series(actor1, actor2, board_size, num_games, epsilon, time_budget, exploration):
@@ -22,15 +21,15 @@ def run_series(actor1, actor2, board_size, num_games, epsilon, time_budget, expl
         player1 = MCTSAgent(board, actor=actors[w], epsilon=epsilon, time_budget=time_budget, exploration=exploration)
         player2 = MCTSAgent(board, actor=actors[b], epsilon=epsilon, time_budget=time_budget, exploration=exploration)
         while not board.winner:
-            move, _ = player1.get_move()
+            move, _, _ = player1.get_move()
             board.place_piece(move)
             if not board.winner:
-                move, _ = player2.get_move()
+                move, _, _ = player2.get_move()
                 board.place_piece(move)
-                board.print_board()
-        print("WINNER", board.winner)
-        print("W=", w, "B=", b)
-        board.print_board()
+                #board.print_board()
+        #print("WINNER", board.winner)
+        #print("W=", w, "B=", b)
+        #board.print_board()
 
         winner = w if board.winner == 1 else b
         statistics[winner] += 1
@@ -45,27 +44,32 @@ def run_series(actor1, actor2, board_size, num_games, epsilon, time_budget, expl
 def run_tournament():
     actors = []
     actors_names = []
-    size = 7
+    size = 4
     actors_dir = f"{size}X{size}"
     for filename in os.listdir(actors_dir):
         f = os.path.join(actors_dir, filename)
         # checking if it is a file
         if os.path.isfile(f):
-            if filename == "game50.h5" or filename == "game300.h5":
+            if filename == "game50.h5" or filename == "game400.h5":
                 print(filename)
                 actors_names.append(filename)
-                model = DQN(size * size)
-                model.load_state_dict(torch.load(f))
-                model.eval()
+
+                model = Anet2(load_path=f)
                 actors.append(model)
 
     for i in range(len(actors)):
         for j in range(i+1, len(actors)):
             print(f"{actors_names[i]} vs {actors_names[j]}")
-            actor1_wins, actor2_wins = run_series(actors[i], actors[j], board_size=size, num_games=20, epsilon=0, time_budget=1,
+            actor1_wins, actor2_wins = run_series(actors[i], actors[j], board_size=size, num_games=20, epsilon=0, time_budget=.2,
                                     exploration=1)
             print(f"{actors_names[i]} victories: {actor1_wins}")
             print(f"{actors_names[j]} victories: {actor2_wins}")
+
+
+def load_saved_model(filename):
+    model = load_model(filename, compile=False)
+    print(f"Model {filename} loaded succesfully")
+    return model
 
 
 run_tournament()
--- nn/anet.py
+++ nn/anet.py
@@ -1,95 +1,30 @@
 import random
 
 import numpy as np
+import keras
 from keras import Input, Model
 from keras.layers import Dense, Embedding, Dropout
+from keras.models import load_model
 from keras.optimizers import Adam
-from torch import nn
-import torch
-
-
-class Anet(nn.Module):
-    def __init__(self, board_size, embedding_size=64):
-        super(Anet, self).__init__()
-        self.nn = nn.Sequential(
-            nn.Linear(board_size**2 + 1, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 364),
-            nn.ReLU(),
-            nn.Linear(364, board_size**2+1)
-        )
-        self.init_weights()
-
-    def forward(self, x):
-        mask = torch.where(x == 0, 1, 0)[:, 1:]
-        output = self.nn(x)
-
-        masked = torch.mul(output, mask)
-        return nn.functional.sigmoid(output)
-
-    def init_weights(self):
-        init_range = 0.2
-        for layer in self.nn:
-            if type(layer) == nn.Linear:
-                layer.bias.data.zero_()
-                layer.weight.data.uniform_(-init_range, init_range)
-
-
-if __name__ == "__main__":
-    board_size = 7
-    embedding_size = 8
-    epochs = 100_000
-    anet = Anet(board_size=board_size, embedding_size=8)
-
-    batch_size = 128
-
-    x_train = []
-    y_train = []
-    for _ in range(batch_size):
-        x_train.append([random.randint(1, 2)] + [random.randint(0, 2) for _ in range(board_size**2)])
-        y_train.append([random.random() for _ in range(board_size**2 + 1)])
-
-    x_train = torch.tensor(x_train, dtype=torch.float32)
-    y_train = torch.tensor(y_train, dtype=torch.float32)
-
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(anet.parameters(), lr=0.001, betas=(0.5, 0.999))
-    #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.999)
-    anet.train()
-
-    for epoch in range(0, epochs + 1):
-        optimizer.zero_grad()
-        y_pred = anet(x_train)
-
-        loss = criterion(y_pred, y_train)
-        if epoch % 100 == 0:
-            print(f"Epoch {epoch} loss: {loss}")
-            #print(scheduler.get_last_lr())
-        loss.backward()
-        optimizer.step()
-        #scheduler.step()
-
-
 
 
 class Anet2:
-    def __init__(self, input_dim, output_dim, hidden_dims=(256, 512, 512, 128), dropout_rate=0.5):
-        input_tensor = Input(shape=input_dim)
-        for i in range(len(hidden_dims)):
-            if i == 0:
-                x = Dense(hidden_dims[i], activation='relu')(input_tensor)
-            else:
-                x = Dense(hidden_dims[i], activation='relu')(x)
-            x = Dropout(dropout_rate)(x)
+    def __init__(self, input_dim=17, output_dim=16, hidden_dims=(256, 512, 512, 128), dropout_rate=0.3, load_path=None):
+        self.load_path = load_path
+        if self.load_path is not None:
+            self.model = self.load_saved_model(load_path)
+        else:
+            input_tensor = Input(shape=(input_dim,))
+            x = Embedding(input_dim=3, output_dim=4, input_length=input_dim)(input_tensor)
 
-        output = Dense(output_dim, activation="softmax")(x)
-        self.model = Model(input_tensor, output)
-        self.model.compile(optimizer=Adam(learning_rate=1e-3), loss="kl_divergence")
-        self.model.summary()
+            for hidden_dim in hidden_dims:
+                x = Dense(hidden_dim, activation='relu')(x)
+                x = Dropout(dropout_rate)(x)
+
+            output = Dense(output_dim, activation="softmax")(x)
+            self.model = Model(input_tensor, output)
+            self.model.compile(optimizer=Adam(learning_rate=1e-3), loss="kl_divergence")
+            self.model.summary()
 
     def predict(self, x):
         mask = np.where(x == 0, 1, 0)[:, 1:]
@@ -109,3 +44,8 @@ class Anet2:
     def save_model(self, name):
         self.model.save(f"{name}.h5")
         print(f"Model {name}.h5 saved succesfully")
+
+    def load_saved_model(self, filename):
+        model = load_model(filename, compile=False)
+        print(f"Model {filename} loaded succesfully")
+        return model
