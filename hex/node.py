78d82b2db9db847f2be99fc1bed86b58ceeee2f6
Simon <simon.jensen@ntnu.no>
2023-03-27 13:26:50 +0200

    Moving node object to seperate file

---
+++ hex/node.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+
+class Node:
+    def __init__(self, move=None, parent=None):
+        self.move = move
+        self.parent = parent
+        self.children = []
+        self.N = 0
+        self.Q = 0
+
+    def value(self, explore):
+        if self.N == 0:
+            if explore == 0:
+                return 0
+            else:
+                return np.inf
+        else:
+            return self.Q / self.N + explore * np.sqrt(np.log(self.parent.N) / self.N)
+
+    def add_children(self, children):
+        self.children += children
