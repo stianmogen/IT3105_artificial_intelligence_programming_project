14378c45ce97e6e300b05eb2747f5f2e3ad206ab
Simon <simon.jensen@ntnu.no>
2023-03-27 13:21:09 +0200

    Refactor and test game state

--- hex/game_state.py
+++ hex/game_state.py
@@ -1,6 +1,8 @@
 import copy
+import random
 
 import numpy as np
+import unittest
 
 from disjoint_set import DisjointSet
 
@@ -13,8 +15,7 @@ class HexGameState:
         self.empty_spaces = set(range(size * size))
         self.column_names = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.last_move = None
-        self.winner = None
-        self.neighbor_pattern = self.generate_neighbour_pattern()
+        self.neighbour_pattern = self.generate_neighbour_pattern()
         self.left_right = DisjointSet(size * size + 2)
         self.top_bottom = DisjointSet(size * size + 2)
 
@@ -34,46 +35,53 @@ class HexGameState:
         return neighbour_patters
 
     def place_piece(self, move):
-        if not self.board[move]:
+        if self.board[move] == 0:
             self.board[move] = self.current_player
             self.last_move = move
             self.empty_spaces.remove(move)
-            if self.check_win(move):
-                self.winner = self.current_player
-            self.current_player = 1 if self.current_player == 2 else 2
-            return True
-        return False
+            self.update_groups(move)
+            self.next_turn()
+        else:
+            raise Exception("Invalid move, tile already placed here")
+
+    def update_groups(self, move):
+        player = self.board[move]
+        if player == 1:
+            for neighbour in self.neighbour_pattern[move]:
+                if self.board[neighbour] == player:
+                    self.left_right.union(neighbour, move)
+        elif player == 2:
+            for neighbour in self.neighbour_pattern[move]:
+                if self.board[neighbour] == player:
+                    self.top_bottom.union(neighbour, move)
+        else:
+            raise Exception("This move has an unassigned player")
+
+    def next_turn(self):
+        self.current_player = 1 if self.current_player == 2 else 2
 
     def neighbours(self, y, x, hex_pattern):
         return [(y + y_, x + x_) for y_, x_ in hex_pattern if
                 (0 <= (y + y_) < self.size) and (0 <= (x + x_) < self.size)]
 
-    def check_win(self, move):
-        player = self.board[move]
-
-        if player == 1:
-            for move_ in self.neighbor_pattern[move]:
-                if self.board[move_] == player:
-                    self.left_right.union(move_, move)
-            return self.left_right.connected(self.size * self.size, self.size * self.size + 1)
-        elif player == 2:
-            for move_ in self.neighbor_pattern[move]:
-                if self.board[move_] == player:
-                    self.top_bottom.union(move_, move)
-            return self.top_bottom.connected(self.size * self.size, self.size * self.size + 1)
-        return False
-
-    def clone_board(self):
-        return copy.deepcopy(self.board)
-
+    @property
+    def winner(self):
+        if self.left_right.connected(self.size * self.size, self.size * self.size + 1):
+            return 1
+        elif self.top_bottom.connected(self.size * self.size, self.size * self.size + 1):
+            return 2
+        else:
+            return 0
 
     def reset_board(self):
         self.current_player = 1
         self.board = np.zeros(self.size * self.size, dtype=int)
         self.empty_spaces = set(range(self.size * self.size))
-        self.last_move = None
-        self.winner = None
+        self.left_right = DisjointSet(self.size * self.size + 2)
+        self.top_bottom = DisjointSet(self.size * self.size + 2)
 
+    def clone_board(self):
+        return copy.deepcopy(self.board)
 
     def print_board(self):
         rows = self.size
@@ -101,3 +109,45 @@ class HexGameState:
             indent += 2
         headings = " " * (indent - 2) + headings
         print(headings)
+
+
+class Test(unittest.TestCase):
+    def test_invalid_input(self):
+        hex_game = HexGameState(5)
+        hex_game.place_piece(0)
+        with self.assertRaises(Exception):
+            hex_game.place_piece(0)
+
+    def test_valid_input(self):
+        hex_game = HexGameState(5)
+        while len(hex_game.empty_spaces) > 0:
+            move = random.choice(tuple(hex_game.empty_spaces))
+            hex_game.place_piece(move)
+
+    def test_winner_horizontal(self):
+        hex_game = HexGameState(5)
+        for i in range(5):
+            self.assertIs(hex_game.winner, 0)
+            hex_game.place_piece(i)  # white
+            hex_game.place_piece(5 + i)  # black
+        self.assertIs(hex_game.winner, 1)
+
+    def test_winner_vertical(self):
+        hex_game = HexGameState(5)
+        for i in range(5):
+            self.assertIs(hex_game.winner, 0)
+            hex_game.place_piece(i * 5)  # white
+            hex_game.place_piece(i * 5 + 1)  # black
+        self.assertIs(hex_game.winner, 2)
+
+    def test_winner(self):
+        hex_game = HexGameState(5)
+        sequence = [0, 24, 6, 18, 12, 13, 17, 7, 11, 1, 5, 19, 10, 3, 4, 20, 8]
+        for move in sequence:
+            self.assertIs(hex_game.winner, 0)
+            hex_game.place_piece(move)
+        self.assertIs(hex_game.winner, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
